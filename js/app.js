2326278ff10e01a93727314fcc4fb6c2ea8aec69
MarkLorenceB <marklorenceberon06@gmail.com>
2023-01-06 13:17:48 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__actions");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  mobileNav.classList.toggle("open");
+});
+AOS.init();
